af4092aad1fdb62841a2c33782d8c8f6d365d14e
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2021-12-20 20:44:56 +0000

    Necesito terapia emocional urgente + CRUD products nearly done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f5787fd44dfdac5840b00c67a363c98e6981493d67dea6b4f2c865938eb052
-size 2175
+oid sha256:69ef85ce610c1c63a3f1ade7bb5869101fdd58117a2dc8563180933a7af30f0a
+size 2326
