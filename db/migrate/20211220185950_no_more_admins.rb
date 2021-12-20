8d62dc5d78523cab518627877b8d1a373520ee73
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2021-12-20 19:01:03 +0000

    Only 1 user is of type admin. Starting product views + CRUD

---
+++ db/migrate/20211220185950_no_more_admins.rb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ab14faf9a31c4fb5dc3fa684292e12b0646fc1a7eccd3e765f90575d3f26f91
+size 135
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:309f178ce8dc4fb5a78d06ee4b71fc7d6a8098ee971ff15a2fc2c1694b3ba2f1
-size 2473
+oid sha256:2d671ba78062f3ce0a183d3822be68a3f469b86fcccf993cf4f423c6a559518e
+size 2474
